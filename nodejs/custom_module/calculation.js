4fa8c77cd1c3b1321fec3ce218c4f59cc857eb20
Vishnu <145321614+vishnu1002@users.noreply.github.com>
2024-04-25 12:53:49 +0530

    Delete nodejs directory

--- nodejs/custom_module/calculation.js
+++
@@ -1,6 +0,0 @@
-const math = require("./math");
-
-console.log("Add     : ", math.add(10,20));
-console.log("Subtract: ", math.sub(10,20));
-console.log("Multiply: ", math.mul(10,20));
-console.log("Divide  : ", math.div(10,20));
